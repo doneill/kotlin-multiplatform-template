b5006126d61e1b77f1d0653e62c77958287466f1
Dan O'Neill <dev@jdoneill.com>
2019-12-06 18:59:54 -0800

    clean up

--- app/src/main/java/com/jdoneill/ktmultiplatform/MainActivity.kt
+++ app/src/main/java/com/jdoneill/ktmultiplatform/MainActivity.kt
@@ -10,6 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         findViewById<TextView>(R.id.date_view).text = getDate()
     }
 }
