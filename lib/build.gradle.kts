3197bebba41e22e09b20b9c34c956d81bf9378ec
Dan O'Neill <dev@jdoneill.com>
2019-12-11 21:54:08 -0800

    add ktlint and init

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -2,10 +2,11 @@ import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 
 plugins {
     kotlin("multiplatform")
+    id("org.jlleitschuh.gradle.ktlint") version "9.1.1"
 }
 
 kotlin {
-    //select iOS target platform depending on the Xcode environment variables
+    // select iOS target platform depending on the Xcode environment variables
     val iOSTarget: (String, KotlinNativeTarget.() -> Unit) -> KotlinNativeTarget =
     if (System.getenv("SDK_NAME")?.startsWith("iphoneos") == true)
     ::iosArm64
@@ -34,9 +35,9 @@ kotlin {
 val packForXcode by tasks.creating(Sync::class) {
     val targetDir = File(buildDir, "xcode-frameworks")
 
-    /// selecting the right configuration for the iOS
-    /// framework depending on the environment
-    /// variables set by Xcode build
+    // / selecting the right configuration for the iOS
+    // / framework depending on the environment
+    // / variables set by Xcode build
     val mode = System.getenv("CONFIGURATION") ?: "DEBUG"
     val framework = kotlin.targets
             .getByName<KotlinNativeTarget>("ios")
@@ -47,13 +48,13 @@ val packForXcode by tasks.creating(Sync::class) {
     from({ framework.outputDirectory })
     into(targetDir)
 
-    /// generate a helpful ./gradlew wrapper with embedded Java path
+    // / generate a helpful ./gradlew wrapper with embedded Java path
     doLast {
         val gradlew = File(targetDir, "gradlew")
-        gradlew.writeText("#!/bin/bash\n"
-                + "export 'JAVA_HOME=${System.getProperty("java.home")}'\n"
-                + "cd '${rootProject.rootDir}'\n"
-                + "./gradlew \$@\n")
+        gradlew.writeText("#!/bin/bash\n" +
+                "export 'JAVA_HOME=${System.getProperty("java.home")}'\n" +
+                "cd '${rootProject.rootDir}'\n" +
+                "./gradlew \$@\n")
         gradlew.setExecutable(true)
     }
 }
