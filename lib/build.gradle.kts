86c5df66db4784fe0a0412bdcf763237112ce61a
Dan O'Neill <dev@jdoneill.com>
2020-06-06 17:35:36 -0700

    set up dependencies

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -2,11 +2,13 @@ import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 
 plugins {
     kotlin("multiplatform")
+    kotlin("plugin.serialization")
     id("org.jlleitschuh.gradle.ktlint") version "9.1.1"
 }
 
 kotlin {
     // select iOS target platform depending on the Xcode environment variables
+    // iPhone simulator : presets.iosX64 | real iDevice 64 bit : presets.iosArm64
     val iOSTarget: (String, KotlinNativeTarget.() -> Unit) -> KotlinNativeTarget =
     if (System.getenv("SDK_NAME")?.startsWith("iphoneos") == true)
     ::iosArm64
@@ -25,10 +27,23 @@ kotlin {
 
     sourceSets["commonMain"].dependencies {
         implementation("org.jetbrains.kotlin:kotlin-stdlib-common")
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-common:1.1.1")
+        implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime-common:0.10.0")
+        implementation("io.ktor:ktor-client-core:1.1.2")
     }
 
     sourceSets["androidMain"].dependencies {
         implementation("org.jetbrains.kotlin:kotlin-stdlib")
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.1.1")
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.1.1")
+        implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:0.10.0")
+        implementation("io.ktor:ktor-client-android:1.1.2")
+    }
+
+    sourceSets["iosMain"].dependencies {
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-native:1.1.1")
+        implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime-native:0.10.0")
+        implementation("io.ktor:ktor-client-ios:1.1.2")
     }
 }
 
