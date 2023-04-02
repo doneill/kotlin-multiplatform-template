123607d129ba3adf11c62661eb4ab5eecc069ae7
Dan O'Neill <dev@jdoneill.com>
2023-04-02 11:28:43 -0700

    update versions

--- buildSrc/src/main/java/Coordinates.kt
+++ buildSrc/src/main/java/Coordinates.kt
@@ -3,6 +3,6 @@ val PUBLISHING_GROUP = "com.jdoneill"
 object AppCoordinates {
     const val APP_ID = "com.jdoneill.ktmultiplatform"
 
-    const val APP_VERSION_NAME = "0.0.1"
+    const val APP_VERSION_NAME = "0.4.0"
     const val APP_VERSION_CODE = 1
 }
